bbb3b2056324e17faaf903a2851297563d6151cc
Jovan Lanik <jox969@gmail.com>
2020-05-18 19:15:26 +0200

    changes for new gpu

--- monitors.sh
+++ monitors.sh
@@ -1,11 +1,7 @@
 !/bin/sh
-if	lsmod | grep -q 'nouveau' && \
-	lsusb | grep -q '17ef:100a'
+if lspci -n | grep -q "1002:67df"
 then
-	xrandr --setprovideroutputsource nouveau Intel
-	xrandr --output LVDS1 --mode 1920x1080 --rate 60 --pos 0x120 \
-	--output DP-1-2 --primary --mode 1920x1200 --rate 60 --pos 1920x0
-
+	xrandr --output DVI-D-0 --primary --mode 1920x1200 --rate 60 --pos 0x0
 else
-	xrandr 	--output LVDS1 --primary --mode 1920x1080 --rate 60 --pos 0x0
+	xrandr --output LVDS1 --primary --mode 1920x1080 --rate 60 --pos 0x0
 fi
