aed91f2c0c661f30202a3ff65320e33e41142626
Jovan Lanik <jox969@gmail.com>
2021-03-06 19:48:34 +0100

    update xmenu

--- xmenu/.local/bin/xmenu-window.sh
+++ xmenu/.local/bin/xmenu-window.sh
@@ -9,11 +9,19 @@ i3 = Connection()
 print(i3.get_tree().find_focused().workspace().name)
 EOF
 )
+workspaces=$(
+python3 << EOF
+from i3ipc import Connection, Event
+i3 = Connection()
+for workspace in i3.get_workspaces(): print(workspace.name)
+EOF
+)
 echo "Switch to"
 for i in $(seq 1 6)
 do
-	[ "$i" = "$current" ] && c=' (current)' || c=''
-	echo "\tWorkspace ${i}${c}\ti3-msg workspace $i"
+	echo "$workspaces" | tr ' ' '\n' | grep -q "$i" && a='-' || a=' '
+	[ "$i" = "$current" ] && a='+'
+	echo "\t$a Workspace ${i}\ti3-msg workspace $i"
 done
 python3 << EOF
 from i3ipc import Connection, Event
@@ -27,11 +35,13 @@ EOF
 echo '\nSend to'
 for i in $(seq 1 6)
 do
-	[ "$i" = "$current" ] && c=' (current)' || c=''
-	echo "\tWorkspace ${i}${c}\ti3-msg move container to workspace $i"
+	echo "$workspaces" | tr ' ' '\n' | grep -q "$i" && a='-' || a=' '
+	[ "$i" = "$current" ] && a='+'
+	echo "\t$a Workspace ${i}\ti3-msg move container to workspace $i"
 done
 
 echo 'Promote	i3-promote-window.py'
+echo 'Swap	i3-msg mark swap && i3-swap.sh drop'
 echo 'Fullscreen	i3-msg fullscreen'
 echo 'Float	i3-msg floating toggle'
 echo
